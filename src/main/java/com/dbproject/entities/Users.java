31798da9b473d9b9f881f47ff4146cf7feca2276
DESKTOP-8EAQC0R\KD8 <kd8@live.dk>
2020-12-08 15:00:56 +0100

    added data for mongoDB

--- src/main/java/com/dbproject/entities/Users.java
+++ src/main/java/com/dbproject/entities/Users.java
@@ -4,30 +4,25 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+
+@Getter
+@Setter
+@ToString
+@Document(collection = "users")
 public class Users {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
     private String userName;
     private Integer rating;
     private String password;
 
-    @ManyToOne //important notation!
-    private Location location; //many user can have one location
-    private String email;
-
-    @OneToMany
-    @JoinColumn(name = "user_id")
-    private Set<Vehicle> vehicles = new HashSet<>();
-
-    public Users() {
-        //needed for JPA
-    }
-    //rating can be null, user start with no cars
     public Users(String userName, String password, Location location, String email) {
         this.userName = userName;
         this.password = password;
@@ -35,94 +30,4 @@ public class Users {
         this.email = email;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getRating() {
-        return rating;
-    }
-
-    public void setRating(Integer rating) {
-        this.rating = rating;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Vehicle> getVehicles() {
-        return vehicles;
-    }
-
-    public void setVehicles(Set<Vehicle> vehicles) {
-        this.vehicles = vehicles;
-    }
-
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = @JoinColumn(name = "users_id"),
-            inverseJoinColumns = @JoinColumn(name = "roles_id")
-    )
-    private Set<Role> roles = new HashSet<>();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Users users = (Users) o;
-        return id.equals(users.id) &&
-                userName.equals(users.userName) &&
-                Objects.equals(rating, users.rating) &&
-                password.equals(users.password) &&
-                location.equals(users.location) &&
-                email.equals(users.email) &&
-                Objects.equals(vehicles, users.vehicles);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, userName, rating, password, location, email, vehicles);
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
